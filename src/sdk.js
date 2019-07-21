120828a87d32aab68fc39df0ca777e2b708e57c6
eldadfux <eldad.fux@gmail.com>
2019-07-21 14:46:28 +0300

    Initial commit

--- src/sdk.js
+++ src/sdk.js
@@ -132,7 +132,7 @@
                 globalParams.push({key: key, value: value});
             };
 
-            addGlobalHeader('x-sdk-version', 'appwrite:javascript:v1.0.4');
+            addGlobalHeader('x-sdk-version', 'appwrite:javascript:v1.0.5');
             addGlobalHeader('content-type', '');
 
             /**

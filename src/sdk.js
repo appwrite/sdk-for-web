5aa65fd57ad6ba07522e8ab8bb032003cc5c9b7d
Vy Dao <vynguyendao@gmail.com>
2019-10-04 23:43:47 +0700

    Correct endpoint in configuration

--- src/sdk.js
+++ src/sdk.js
@@ -2,7 +2,7 @@
     window.Appwrite = function () {
 
         let config = {
-            endpoint: 'https://https://appwrite.io/v1',
+            endpoint: 'https://appwrite.io/v1',
             project: '',
             key: '',
             locale: '',

ae61bdd01086b78e6b0741db94583d69c54b13e1
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-05-21 17:43:42 +0200

    fix-travis-release (#16)
    * Update package.json
    
    * Update .travis.yml
    
    * Update README.md
    
    * Update sdk.ts

--- src/sdk.ts
+++ src/sdk.ts
@@ -29,7 +29,7 @@ class Appwrite {
         locale: '',
     };
     headers: Headers = {
-        'x-sdk-version': 'appwrite:web:3.0.2',
+        'x-sdk-version': 'appwrite:web:3.0.3',
         'X-Appwrite-Response-Format': '0.8.0',
     };
 

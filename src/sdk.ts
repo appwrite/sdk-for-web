2a3bc2bdc936e11500f6970f7c68caa333e835af
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-01-05 15:47:45 +0100

    fix: add query builder

--- src/sdk.ts
+++ src/sdk.ts
@@ -791,7 +791,7 @@ class Appwrite {
         locale: '',
     };
     headers: Headers = {
-        'x-sdk-version': 'appwrite:web:6.0.0',
+        'x-sdk-version': 'appwrite:web:6.0.1',
         'X-Appwrite-Response-Format': '0.12.0',
     };
 
@@ -3504,5 +3504,44 @@ class Appwrite {
 
 };
 
-export { Appwrite }
-export type { AppwriteException, Models }
+type QueryTypesSingle = string | number | boolean;
+type QueryTypesList = string[] | number[] | boolean[];
+type QueryTypes = QueryTypesSingle | QueryTypesList;
+
+class Query {
+  static equal = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "equal", value);
+
+  static notEqual = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "notEqual", value);
+
+  static lesser = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "lesser", value);
+
+  static lesserEqual = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "lesserEqual", value);
+
+  static greater = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "greater", value);
+
+  static greaterEqual = (attribute: string, value: QueryTypes): string =>
+    Query.addQuery(attribute, "greaterEqual", value);
+
+  static search = (attribute: string, value: string): string =>
+    Query.addQuery(attribute, "search", value);
+
+  private static addQuery = (attribute: string, oper: string, value: QueryTypes): string =>
+    value instanceof Array
+      ? `${attribute}.${oper}(${value
+          .map((v: QueryTypesSingle) => Query.parseValues(v))
+          .join(",")})`
+      : `${attribute}.${oper}(${Query.parseValues(value)})`;
+
+  private static parseValues = (value: QueryTypes): string =>
+    typeof value === "string" || value instanceof String
+      ? `"${value}"`
+      : `${value}`;
+}
+
+export { Appwrite, Query }
+export type { AppwriteException, Models, QueryTypes, QueryTypesList }

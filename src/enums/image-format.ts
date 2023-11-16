598469cb865fe95fa31bc3ac663f047d6463e45f
Jake Barnby <jakeb994@gmail.com>
2023-11-16 17:08:25 +1300

    Fix between query output

--- src/enums/image-format.ts
+++
@@ -1,7 +0,0 @@
-export enum ImageFormat {
-    Jpg = 'jpg',
-    Jpeg = 'jpeg',
-    Gif = 'gif',
-    Png = 'png',
-    Webp = 'webp',
-}
--- src/enums/method.ts
+++
@@ -1,8 +0,0 @@
-export enum Method {
-    GET = 'GET',
-    POST = 'POST',
-    PUT = 'PUT',
-    PATCH = 'PATCH',
-    DELETE = 'DELETE',
-    OPTIONS = 'OPTIONS',
-}

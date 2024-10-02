6c92f6425626cf980a82fa319b7be86b4d3dbbe4
loks0n <22452787+loks0n@users.noreply.github.com>
2024-10-02 22:38:04 +0100

    fix(web): credentials include

--- src/client.ts
+++ src/client.ts
@@ -303,7 +303,7 @@ class Client {
         'x-sdk-name': 'Web',
         'x-sdk-platform': 'client',
         'x-sdk-language': 'web',
-        'x-sdk-version': '16.0.1',
+        'x-sdk-version': '16.0.2',
         'X-Appwrite-Response-Format': '1.6.0',
     };
 
@@ -589,6 +589,7 @@ class Client {
         let options: RequestInit = {
             method,
             headers,
+            credentials: 'include',
         };
 
         if (method === 'GET') {

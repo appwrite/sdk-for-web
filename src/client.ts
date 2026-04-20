1469c61305742538deb6b8fd7ca70ae4ba66d7ca
root <arnabchatterjee.ac.2@gmail.com>
2026-04-20 08:19:59 +0000

    chore: update Web SDK to 24.2.0

--- src/client.ts
+++ src/client.ts
@@ -399,7 +399,7 @@ class Client {
         'x-sdk-platform': 'client',
         'x-sdk-language': 'web',
         'x-sdk-version': '24.2.0',
-        'X-Appwrite-Response-Format': '1.9.1',
+        'X-Appwrite-Response-Format': '1.9.0',
     };
 
     /**

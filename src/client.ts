837239a4dff5070617568cc0fc0d94f057c88a82
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-07-13 12:09:19 +0530

    chore: update Web SDK to 26.2.0

--- src/client.ts
+++ src/client.ts
@@ -481,7 +481,7 @@ class Client {
      * @return {this}
      */
     setBearer(value: string): this {
-        this.headers['Authorization'] = value;
+        this.headers['Authorization'] = `Bearer ${value}`;
         this.config.bearer = value;
         return this;
     }
@@ -708,19 +708,19 @@ class Client {
                     case 'connected': {
                         const messageData = <RealtimeResponseConnected>message.data;
 
-                            let session = this.config.session;
-                            if (!session) {
-                                const cookie = JSONbig.parse(window.localStorage.getItem('cookieFallback') ?? '{}');
-                                session = cookie?.[`a_session_${this.config.project}`];
-                            }
-                            if (session && !messageData?.user) {
-                                this.realtime.socket?.send(JSONbig.stringify(<RealtimeRequest>{
-                                    type: 'authentication',
-                                    data: {
-                                        session
-                                    }
-                                }));
-                            }
+                        let session = this.config.session;
+                        if (!session) {
+                            const cookie = JSONbig.parse(window.localStorage.getItem('cookieFallback') ?? '{}');
+                            session = cookie?.[`a_session_${this.config.project}`];
+                        }
+                        if (session && !messageData?.user) {
+                            this.realtime.socket?.send(JSONbig.stringify(<RealtimeRequest>{
+                                type: 'authentication',
+                                data: {
+                                    session
+                                }
+                            }));
+                        }
 
                         this.realtime.subscriptions.forEach((sub, subscriptionId) => {
                             this.realtime.pendingSubscribes.set(subscriptionId, {
